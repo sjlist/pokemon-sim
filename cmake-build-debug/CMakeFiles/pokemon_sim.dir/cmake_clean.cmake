fbe53da56165c0ba819aa45c2b2d2255536547a1
Sam List <samjlist@yahoo.com>
2018-12-13 07:28:23 -0500

    Added cmake debug support

---
+++ cmake-build-debug/CMakeFiles/pokemon_sim.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pokemon_sim.dir/main.cpp.o"
+  "CMakeFiles/pokemon_sim.dir/Move.cpp.o"
+  "CMakeFiles/pokemon_sim.dir/Pokemon.cpp.o"
+  "CMakeFiles/pokemon_sim.dir/Type.cpp.o"
+  "pokemon_sim.pdb"
+  "pokemon_sim"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pokemon_sim.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
