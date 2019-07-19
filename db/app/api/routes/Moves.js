62dd52afe9e2c064553ae23e3ce3994f29847e80
Alton Barbehenn <altonbarbehenn@gmail.com>
2019-07-19 12:05:48 -0500

    added error handling to post

--- db/app/api/routes/Moves.js
+++ db/app/api/routes/Moves.js
@@ -17,21 +17,23 @@ router.get('/', (req, res) => {
 router.post('/', (req, res) => {
 	let newMove = new MoveModel(req.body);
 	console.log(newMove);
-	newMove.save();
+	newMove.save(function (err) {
+		if (err) res.send(err);
+	});
 	res.send(newMove);
 });
 
 router.delete('/', (req, res) => {
 	console.log('delete recieved:', req.body);
 	MoveModel.deleteOne({_id: req.body._id}, function (err) {
-		if (err) console.log(err);
+		if (err) res.send(err);
 	});
 	res.send(req.body);
 });
 
 router.put('/', (req, res) => {
 	MoveModel.updateOne({_id: req.body._id}, req.body, function (err) {
-		if (err) console.log(err);
+		if (err) res.send(err);
 	});
 	res.send(req.body);
 	console.log('recieved update:', req.body);
