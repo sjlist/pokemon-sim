b589842947592d54cf1243637ca64a2cc2329dee
sjlist <samjlist@yahoo.com>
2019-10-28 15:05:22 -0400

    Implemented coverage reports

--- Config.h
+++ Config.h
@@ -15,13 +15,13 @@ const string PROJECT_DIRECTORY = "/home/slist/projects/pokemon-sim/";
 #define BATTLE_TYPE DOUBLE_BATTLE
 #define MAX_TURN_COUNT 400
 
-#define RAND_SEED 508941264
+#define RAND_SEED 1758096715
 
 #define DEBUG_LEVEL 1
 
 #if DEBUG_LEVEL >= 4
 #define DEBUGGING
-#define DEBUG_TURN 17
+#define DEBUG_TURN 25
 #endif
 
 #endif /* POKEMON_SIM_CONFIG_H */
